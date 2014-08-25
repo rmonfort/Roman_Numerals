20a3d7829d595896f4b5333545eced17f4c2818f
Richard S. Monfort <richard.s.monfort@gmail.com>
2014-08-25 17:32:57 -0400

    Added source and example input file

---
+++ Roman_Numerals/Source.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::ifstream;
+using std::getline;
+using std::ostringstream;
+
+int main(int argc, char *argv[])
+{
+	string input_file;
+
+	// Check first argument if it exists for input file, otherwise ask user
+	if (argc > 1)
+	{
+		input_file = argv[1];
+	}
+	else
+	{
+		cout << "Please enter the path to your input file: " << endl;
+		cin >> input_file;
+	}
+
+	// Open input file and check if successfully opened
+	ifstream ifs(input_file);
+	if (!ifs)
+	{
+		cout << "Failed to open input file." << endl;
+		return 1;
+	}
+
+	int number = 0;
+	// Read number from input file and convert to roman numeral
+	while (ifs >> number)
+	{
+		ostringstream roman_numeral; // Holds output till number is fully converted
+		while (number > 0)
+		{
+			// 1000 - M
+			if (number >= 1000)
+			{
+				roman_numeral << "M";
+				number -= 1000;
+				continue; // For multiples of 1000
+			}
+			// 900 - CM
+			if (number >= 900)
+			{
+				roman_numeral << "CM";
+				number -= 900;
+			}
+			// 500 - D
+			if (number >= 500)
+			{
+				roman_numeral << "D";
+				number -= 500;
+			}
+			// 400 - CD
+			if (number >= 400)
+			{
+				roman_numeral << "CD";
+				number -= 400;
+			}
+			// 100 - C
+			if (number >= 100)
+			{
+				roman_numeral << "C";
+				number -= 100;
+				continue; // For multiples of 100
+			}
+			// 90 - XC
+			if (number >= 90)
+			{
+				roman_numeral << "XC";
+				number -= 90;
+			}
+			// 50 - L
+			if (number >= 50)
+			{
+				roman_numeral << "L";
+				number -= 50;
+			}
+			// 40 - XL
+			if (number >= 40)
+			{
+				roman_numeral << "XL";
+				number -= 40;
+			}
+			// 10 - X
+			if (number >= 10)
+			{
+				roman_numeral << "X";
+				number -= 10;
+			}
+			// 9 - IX
+			if (number == 9)
+			{
+				roman_numeral << "IX";
+				number -= 9;
+			}
+			// 5 - V
+			if (number >= 5)
+			{
+				roman_numeral << "V";
+				number -= 5;
+			}
+			// 4 - IV
+			if (number == 4)
+			{
+				roman_numeral << "IV";
+				number -= 4;
+			}
+			// 1 - I
+			if (number >= 1)
+			{
+				roman_numeral << "I";
+				number -= 1;
+			}
+		}
+		// Output completed roman numeral 
+		cout << roman_numeral.str() << endl;
+	}
+	return 0;
+}
